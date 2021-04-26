4623e1b3e76d5c455491f70c91efe822505bc2c9
SzJackiewicz <sz.jackiewicz@gmail.com>
2021-04-26 10:37:57 +0200

    rozwiązane zadania

--- 06_Funkcje/05_Powtarzajaca_sie_wartosc/js/app.js
+++ 06_Funkcje/05_Powtarzajaca_sie_wartosc/js/app.js
@@ -1,5 +1,23 @@
 const numbers = [2, 4, 5, 2, 3, 5, 1, 2, 4];
 
+function indexOfRepeatedValue (array) {
+    let firstIndex;
+    for (let i = 1; i < array.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (array[i] === array[j]){
+                firstIndex = j;
+                break;
+            }
+        }
+        if (firstIndex !== undefined){
+            break;
+        }
+    }
+    console.log(firstIndex);
+    return firstIndex;
+}
+
+indexOfRepeatedValue(numbers);
 /**
  * Write your code below!
  */
