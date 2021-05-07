9aea1f663421a5ba88de476ff6a6e2e323c07658
SzJackiewicz <sz.jackiewicz@gmail.com>
2021-05-07 08:55:05 +0200

    rozwiązane zadania

--- 06_Funkcje/04_Wartosc_maksymalna/js/app.js
+++ 06_Funkcje/04_Wartosc_maksymalna/js/app.js
@@ -1,8 +1,8 @@
 const randomNumbers = [27, 64, 47, 78, 48, 11, 76, 25, 11, 83];
 
-function maxFromArray (numbers){
-   let max = Math.max(...numbers);
-   return max;
+function maxFromArray(numbers) {
+  let max = Math.max(...numbers);
+  return max;
 }
 
 let result = maxFromArray(randomNumbers);
